196ca6db68393f3dc05c78f4be2bdbf684acd474
Esteban Cáceres Tapia <e.cacerestapia@uandresbello.edu>
2019-08-31 21:41:49 -0400

    Refactoring de código, primera aproximación a espera de testeo

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,265 +1,66 @@
-import org.jivesoftware.smack.SmackException;
-import org.jivesoftware.smack.XMPPException;
-import org.jivesoftware.smack.chat2.Chat;
-import org.jivesoftware.smack.chat2.IncomingChatMessageListener;
-import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.packet.Presence;
-import org.jivesoftware.smack.roster.Roster;
-import org.jivesoftware.smack.roster.RosterListener;
-import org.jivesoftware.smack.roster.SubscribeListener;
-import org.jxmpp.jid.EntityBareJid;
-import org.jxmpp.jid.Jid;
-import org.jxmpp.stringprep.XmppStringprepException;
-
-import java.io.*;
-import java.util.Collection;
+import org.jivesoftware.smack.ConnectionListener;
+import java.io.File;
+import java.io.IOException;
 
 public class App {
 
-    static File file = new File(System.getProperty("user.dir") + "/config.properties"); //se genera archivo de propiedades
-    static XmppClient xmppClient = null; //se instancia la clase para los metodos de xmpp
-    static PropertiesManager propertiesManager = null; //se instancia la clase para manejar las propiedades
-    static boolean checkOwner = false; //se genera booleando para verificar estatus de dueño
+    private static File file = new File(System.getProperty("user.dir") + "/config.properties"); //instancia de archivo de propiedades.
+    private static XmppClient xmppClient = null; //instancia de la clase de métodos XMPP.
+    private static PropertiesManager propertiesManager = null; //instancia de la clase de manejo de propiedades.
+    private static boolean checkOwner = false;
 
     public static void main(String[] args) {
-        //datos en duro para la conexión al servidor xmpp
+        //datos en duro para la conexión al servidor XMPP
         String domain = "10.40.5.9";
         String user = "estacion";
         String password = "estacion";
 
-        //INICIO conexión y generación de cuenta en XMPP
-        if (!file.exists()) {
+        if (!file.exists()){
             try {
                 xmppClient = new XmppClient(domain, user, password);
-
-                if (xmppClient.getConnection().isConnected()) {
-                    xmppClient.generateAccount();
-                    propertiesManager = new PropertiesManager(domain, user, password);
-                }
-
+                propertiesManager = new PropertiesManager(domain, user, password);
             } catch (Exception e) {
                 file.delete();
-                System.out.println("ERROR: No se pudo generar la conexión al servidor");
-                System.exit(-1); //matando el servicio
+                System.out.println("ERROR: No se pudo generar conexión XMPP por primera vez");
                 e.printStackTrace();
+                System.exit(-1);
             }
-        }
-        //FIN conexión y generación de cuenta en XMPP
 
-        //INICIO carga de configuración e login de cuenta en servidor XMPP
-        if (file.exists() && !file.isDirectory()) {
             try {
-                if (xmppClient == null) {
-                    propertiesManager = new PropertiesManager();
-                    xmppClient = new XmppClient(
-                            propertiesManager.getDomain(),
-                            propertiesManager.getUser(),
-                            propertiesManager.getPassword()
-                    );
-                }
-
-                xmppClient.logInAccount("test");
-
-                checkOwner = propertiesManager.checkOwner();
-
-                if (checkOwner) {
-                    xmppClient.getRoster().setSubscriptionMode(Roster.SubscriptionMode.manual);
-                    System.out.println("INFO: subcription mode manual");
-                } else {
-                    xmppClient.getRoster().setSubscriptionMode(Roster.SubscriptionMode.accept_all);
-                    System.out.println("INFO: subcription mode accept all");
-                }
-
+                xmppClient.generateAccount();
             } catch (Exception e) {
-                System.out.println("ERROR: No se pudo logear en el servidor");
-                System.exit(-1); //matando el servicio
+                file.delete();
+                System.out.println("ERROR: No se pudo generar la cuenta en el servidor");
                 e.printStackTrace();
+                System.exit(-1);
             }
-        }
-        //FIN carga de configuración e login de cuenta en servidor XMPP
 
-
-        //INICIO Listener para subscripción de cuentas
-        try {
-            xmppClient.getRoster().addRosterListener(new RosterListener() {
-                @Override
-                public void entriesAdded(Collection<Jid> addresses) {
-                    System.out.println("INFO: El jid " + addresses + " a sido agregado al roster");
-                    if (!checkOwner) {
-                        for (Jid recorredor : addresses) {
-                            try {
-                                xmppClient.subscribeOwner(recorredor.asBareJid());
-                                System.out.println("INFO: El jid " + addresses + "está siendo configurado como OWNER");
-                                System.out.println("INFO: " + recorredor.toString());
-                                propertiesManager.setOwner(recorredor.toString());
-
-                                if (checkOwner) {
-                                    xmppClient.getRoster().setSubscriptionMode(Roster.SubscriptionMode.manual);
-                                }
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                                System.exit(-1); //matando el servicio
-                            }
-                        }
-                    } else {
-                        for (Jid recorredor : addresses) {
-                            boolean checkWhiteList = propertiesManager.checkWhiteList(recorredor.toString());
-                            if (checkWhiteList) {
-                                try {
-                                    xmppClient.subscribeRequest(recorredor.asBareJid());
-                                } catch (Exception e) {
-                                    e.printStackTrace();
-                                    System.exit(-1); //matando el servicio
-                                }
-                            } else {
-
-                            }
-                        }
-                    }
-                }
-
-                @Override
-                public void entriesUpdated(Collection<Jid> addresses) {
-
-                }
-
-                @Override
-                public void entriesDeleted(Collection<Jid> addresses) {
-
-                }
-
-                @Override
-                public void presenceChanged(Presence presence) {
-
-                    System.out.println("INFO: " + presence);
-                }
-            });
-        } catch (Exception e) {
-            System.out.println("ERROR: cerrando");
-            e.printStackTrace();
-            System.exit(-1); //matando el servicio
-        }
-        //FIN Listener para subscripción de cuentas
-
-        //INICIO Listener de chat
-        try {
-            xmppClient.getChatmanager().addIncomingListener(new IncomingChatMessageListener() {
-                @Override
-                public void newIncomingMessage(EntityBareJid from, Message message, Chat chat) {
-                    System.out.println("<IN: " + message.getBody());
-                    System.out.println("<FROM: " + from);
-
-                    Message respuesta;
-
-                    if (message.getBody().equalsIgnoreCase("datos")) {
-
-                        ReadCSV readCSV = new ReadCSV();
-                        String[] lastData = null;
-                        try {
-                            lastData = readCSV.readData();
-                        } catch (Exception e) {
-                            System.out.println("ERROR: algo paso en el chatmanager");
-                            e.printStackTrace();
-                            System.exit(-1); //matando el servicio
-                        }
-
-                        respuesta = new Message(message.getTo(),
-                                "DateTime: " + lastData[0] +
-                                        "\nDirección del viento: " + lastData[1] +
-                                        "\nVelocidad del viento: " + lastData[2] + " Km/h" +
-                                        "\nLluvia acumulada: " + lastData[3] + " mm" +
-                                        "\nPresión atmosférica: " + lastData[4] + " hPa" +
-                                        "\nHumedad ambiental: " + lastData[5] + " %" +
-                                        "\nTemperatura: " + lastData[6] + " °C" +
-                                        "\nConcentración de CO: " + lastData[10] +
-                                        "\nConcentración de SO2: " + lastData[11] +
-                                        "\nConcentración de NO2: " + lastData[12]
-                        );
-
-                        respuesta.setType(Message.Type.chat);
-                        System.out.println(">OUT: " + respuesta.toString());
-                        try {
-                            chat.send(respuesta);
-                        } catch (Exception e) {
-                            System.out.println("ERROR: algo paso en la respuesta del chatmanager");
-                            e.printStackTrace();
-                            System.exit(-1); //matando el servicio
-                        }
-                    } else if (message.getBody().equalsIgnoreCase("log")) {
-                        try {
-                            xmppClient.transferFile(message.getFrom());
-                            System.out.println("INFO: tratando de enviar archivo a" + message.getFrom());
-                        } catch (Exception e) {
-                            System.out.println("ERROR: algo pasó en el transferfile");
-                            e.printStackTrace();
-                            System.exit(-1); //matando el servicio
-                        }
-
-                    }
-
-                    if (from.toString().equals(propertiesManager.getOwner())) {
-                        if (message.getBody().startsWith("AGREGAR:")) {
-                            String[] texto = message.getBody().split(":");
-                            String jid = texto[1].trim();
-                            try {
-                                propertiesManager.addWhiteList(jid);
-                            } catch (Exception e) {
-                                System.out.println("ERROR: algo paso al agregar a la lista blanca");
-                                e.printStackTrace();
-                                System.exit(-1); //matando el servicio
-                            }
-
-                            respuesta = new Message(message.getTo(),
-                                    "el usuario " + jid + "ha sido agregado a tu lista blanca con éxito");
-
-                            respuesta.setType(Message.Type.chat);
-
-                            try {
-                                chat.send(respuesta);
-                            } catch (Exception e) {
-                                System.out.println("ERROR: No se pudo enviar respuesat de whitelist");
-                                e.printStackTrace();
-                            }
-                        }
-                    }
-
-                }
-            });
-
-        } catch (Exception e) {
-            System.out.println("ERROR: en la wea grande");
-            e.printStackTrace();
-            System.exit(-1); //matando el servicio
-        }
-
-        if (checkOwner) {
+        } else {
             try {
-                xmppClient.getRoster().addSubscribeListener(new SubscribeListener() {
-                    @Override
-                    public SubscribeAnswer processSubscribe(Jid from, Presence subscribeRequest) {
-                        SubscribeAnswer subscribeAnswer;
-                        boolean checkWhiteList = propertiesManager.checkWhiteList(from.toString());
-                        if (checkWhiteList) {
-                            subscribeAnswer = SubscribeAnswer.Approve;
-                        } else {
-                            subscribeAnswer = SubscribeAnswer.Deny;
-                        }
-                        return subscribeAnswer;
-                    }
-                });
-            } catch (Exception e) {
-                System.out.println("ERROR: send subcribe");
+                propertiesManager = new PropertiesManager();
+            } catch (IOException e) {
+                System.out.println("ERROR: Error al cargar archivo de configuración");
                 e.printStackTrace();
-                System.exit(-1); //matando el servicio
+                System.exit(-1);
             }
-        }
-        //FIN Listener de chat
 
-        while (true) {
-            if (!xmppClient.getConnection().isConnected() || !xmppClient.getConnection().isAuthenticated()) {
-                System.out.println("ERROR: Se me jue la wea");
-                break;
+            try {
+                xmppClient = new XmppClient(
+                        propertiesManager.getDomain(),
+                        propertiesManager.getUser(),
+                        propertiesManager.getPassword()
+                );
+            } catch (Exception e) {
+                System.out.println("ERROR: No se pudo conectar a al servidor XMPP");
+                e.printStackTrace();
+                System.exit(-1);
             }
         }
+
+        checkOwner = propertiesManager.checkOwner();
+        xmppClient.connectionListener(checkOwner);
+
+
+
     }
 }
--- src/main/java/PropertiesManager.java
+++ src/main/java/PropertiesManager.java
@@ -1,138 +1,75 @@
 import java.io.*;
 import java.util.Properties;
 
-public class PropertiesManager {
+class PropertiesManager {
     private Properties properties = new Properties();
     private String domain, user, password, owner, whiteList;
     private OutputStream outputStream;
     private InputStream inputStream;
 
-    public String getDomain() {
+    String getDomain() {
         return domain;
     }
 
-    public String getUser() {
+    String getUser() {
         return user;
     }
 
-    public String getPassword() {
+    String getPassword() {
         return password;
     }
 
-    public String getOwner(){
-        return owner;
-    }
-
-    public PropertiesManager() throws IOException {
-        inputStream = new FileInputStream(System.getProperty("user.dir") + "/config.properties");
-        properties.clear();
-        properties.load(inputStream);
-
-        domain = properties.getProperty("dominio");
-        user = properties.getProperty("usuario");
-        password = properties.getProperty("password");
-
-        inputStream.close();
-    }
-
-    public PropertiesManager(String domain, String user, String password) throws IOException {
+    PropertiesManager(String domain, String user, String password) throws IOException {
         this.domain = domain;
         this.user = user;
         this.password = password;
 
         outputStream = new FileOutputStream(System.getProperty("user.dir") + "/config.properties");
-        System.out.println("Creando archivo de configuración " + System.getProperty("user.dir") + "/config.properties");
+        System.out.println("INFO: Creando archivo de configuración");
 
         properties.setProperty("dominio", domain);
         properties.setProperty("usuario", user);
         properties.setProperty("password", password);
         properties.store(outputStream, "CONFIGURACION DE REGISTRO");
 
-        closeOutputstream();
+        outputStream.close();
     }
 
-    private void closeOutputstream() throws IOException {
-        if (outputStream != null) {
-            outputStream.close();
-        }
-    }
-
-    public void setOwner(String owner) throws IOException {
-        this.owner = owner;
-        System.out.println("INFO: Seteando a owner...");
-        outputStream = new FileOutputStream(System.getProperty("user.dir") + "/config.properties");
-        properties.setProperty("owner", owner);
-        properties.store(outputStream, "CONFIGURACION DE REGISTRO CON OWNER");
-        closeOutputstream();
-    }
-
-    public boolean checkOwner() throws IOException {
+    PropertiesManager() throws IOException {
         inputStream = new FileInputStream(System.getProperty("user.dir") + "/config.properties");
-        properties.clear();
         properties.load(inputStream);
-        this.owner = properties.getProperty("owner");
+
+        this.domain = properties.getProperty("dominio");
+        this.user = properties.getProperty("usuario");
+        this.password = properties.getProperty("password");
+
         inputStream.close();
-        if (this.owner == null){
-            System.out.println("INFO: Owner no asignado");
-            return false;
-        } else {
-            System.out.println("INFO: Owner asignado: " + owner);
-            return true;
-        }
     }
 
-    public void addWhiteList(String jid) throws Exception {
-        loadWhiteList();
-
-        outputStream = new FileOutputStream(System.getProperty("user.dir") + "/config.properties");
-
-        if (whiteList == null){
-            whiteList = jid.concat(";");
-            properties.setProperty("listaBlanca", whiteList);
-            properties.store(outputStream, "CONFIGURACION DE REGISTRO CON OWNER Y LISTA BLANCA");
-
-        } else {
-            String[] helper = whiteList.split(";");
-            for (int i=0; i < helper.length; i++){
-                if (helper[i].equals(jid)){
-                    properties.setProperty("listaBlanca", whiteList);
-                    properties.store(outputStream, "CONFIGURACION DE REGISTRO CON OWNER Y LISTA BLANCA");
-                    closeOutputstream();
-                    throw new Exception("ERROR: JID " + jid + " ya ingresada en la lista blanca");
-                }
-            }
-            whiteList = whiteList.concat(jid + ";");
-            properties.setProperty("listaBlanca", whiteList);
-            properties.store(outputStream, "CONFIGURACION DE REGISTRO CON OWNER Y LISTA BLANCA");
-        }
-
-        System.out.println("INFO: Agregando JID: " + jid + " a la lista blanca");
-
-        closeOutputstream();
-    }
-
-    private void loadWhiteList(){
+    private void loadOwner() {
         try {
             inputStream = new FileInputStream(System.getProperty("user.dir") + "/config.properties");
             properties.clear();
             properties.load(inputStream);
-            whiteList = properties.getProperty("listaBlanca");
+
+            this.owner = properties.getProperty("owner");
+
             inputStream.close();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
         } catch (IOException e) {
+            System.out.println("ERROR: No se pudo cargar archivo de configuración");
             e.printStackTrace();
         }
+
     }
 
-    public boolean checkWhiteList(String jid){
-        loadWhiteList();
-        String[] helper = whiteList.split(";");
-        for (int i=0; i < helper.length; i++){
-            if (helper[i].equals(jid)){
-                return true;
-            }
+    boolean checkOwner() {
+        loadOwner();
+        if (this.owner == null){
+            System.out.println("INFO: Owner no asignado");
+            return false;
+        } else {
+            System.out.println("INFO: Owner asignado: " + this.owner);
+            return true;
         }
-        return false;
     }
 }
